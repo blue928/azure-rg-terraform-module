bdc3a70767bdb0faad6f2285c73144278e7bc2bb
Blue Presley <bluepresley@gastonia.com>
2022-03-08 06:20:18 -0500

    add required variables

--- variables.tf
+++ variables.tf
@@ -1,1 +1,9 @@
+variable "default_resource_group_name" {
+    description = "Default resource group name"
+    type = string
+}
 
+variable "location" {
+    description = "Default resource group location"
+    type = string
+}
