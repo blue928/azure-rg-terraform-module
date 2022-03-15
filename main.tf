390868f195182630f28dee2ccbae85c85bdb7ba9
Blue Presley <bluepresley@gastonia.com>
2022-03-15 11:31:05 -0400

    test switch to data source

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "default" {
+#resource "azurerm_resource_group" "default" {
+#  name     = var.resource_group_name
+#  location = var.resource_group_location
+#}
+
+data "azurerm_resource_group" "default" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
-output "resource_group_name" {
+/*output "resource_group_name" {
   value = azurerm_resource_group.default.name
 }
 
 output "resource_group_location" {
   value = azurerm_resource_group.default.location
+}*/
+
+output "resource_group_name" {
+  value = data.azurerm_resource_group.default.name
+}
+
+output "resource_group_location" {
+  value = data.azurerm_resource_group.default.location
 }
